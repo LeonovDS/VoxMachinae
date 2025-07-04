cccbaee402f26823a515eaf3690960640f40dc5a
Daniil Leonov <yshmgrt@gmail.com>
2025-07-04 10:55:12 +0300

    feat: migrate to amper

--- domain/sample/test/SampleTest.kt
+++ domain/sample/test/SampleTest.kt
@@ -1,10 +1,9 @@
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlin.test.Test
 
-class SampleTest : FunSpec() {
-    init {
-        test("1 + 1 should equal 2") {
-            1 + 1 shouldBe 2
-        }
+class SampleTest {
+    @Test
+    fun `just check if tests run properly`() {
+        1 + 1 shouldBe 2
     }
 }
